bbcf8bd2ae01a884309c9f39831e1da3b74c97dc
Erwan31 <spilmonterwan@gmail.com>
2020-08-19 16:47:59 +0100

    Store file and see about blocking/nnblk dinyamic

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+/*
 const os = require('os');
+const fs = require('fs');
 
 let user = os.platform();
+let date = new Date();
 
-console.log(user);
+let message = `User platform "${user}" on ${date} WRITE`;
+
+fs.appendFile("hello.txt", message, function(err){
+    if(err){
+        console.log('not able to append');
+    }
+})
+*/
+
+const fs = require('fs');
+
+const userData = require('./user.js');
+
+console.log(userData);
+
+const data = fs.readFile('./hello.txt', 'utf8', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+});
+
+
+function showMessage() {
+    console.log('Finish');
+}
+
+showMessage();
